4c84da6427a6c07bb311f8f204f44f4162072dd2
AgatheTheCode <agathe.dufour.marin@gmail.com>
2022-08-12 17:07:55 +0200

    Ajout table
    Ajout table éditeur et contenu + base html

---
+++ book_shelf/app/Models/isbn_Livre.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class isbn_Livre extends Model
+{
+    use HasFactory;
+}
